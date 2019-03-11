49fcb8d473a358fd2fe1878fd59da2f70de0ef72
allen <njuallen@foxmail.com>
2019-03-11 11:51:45 +0800

    Added simple exception handler to tracebuf-demo.

--- apps/tracebuffer-demo/src/demo.c
+++ apps/tracebuffer-demo/src/demo.c
@@ -15,7 +15,19 @@ void trace_flowthrough_test();
 void init_stack_test();
 void stack_test();
 
+#define CSR_MCAUSE 0x342
+
+// exception handler
+extern void register_exception_handler();
+void my_exception_handler() {
+  long long mcause = read_csr(CSR_MCAUSE);
+  print_llx(mcause);
+}
+
 int main() {
+  register_exception_handler();
+  // trigger invalid instruction exception, to see how it goes
+  asm volatile (".word 0x0000");
   init_stack_test();
 
   // test instruction trace buffer
